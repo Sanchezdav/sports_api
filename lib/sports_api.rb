c24983d44aa7af75acaa851147eb292e7197204e
David Sanchez <sanchez.dav90@gmail.com>
2024-03-20 21:29:41 -0600

    Initial commit

---
+++ lib/sports_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sports_api/version"
+
+module SportsApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sports_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SportsApi
+  VERSION = "0.1.0"
+end
